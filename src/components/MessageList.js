052cd15605ede2289c87a2647ceb40f20915e0b6
Lexi Zgud <lexibitesblog@gmail.com>
2018-06-30 15:41:47 -0500

    Can not send messages yet

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 
 class MessageList extends Component {
   constructor(props) {
     super(props);
     this.state = {
      messages: [],
+     user: '',
+     content: '',
+     sentAt: '',
+     roomId: '',
+     newMessageContent: ''
 };
+this.handleMessageChange = this.handleMessageChange.bind(this);
+this.handleChange = this.handleChange.bind(this);
 this.messagesRef = this.props.firebase.database().ref('Messages');
+this.createMessage = this.createMessage.bind(this);
 };
 
 componentDidMount() {
@@ -19,6 +28,29 @@ componentDidMount() {
      });
 }
 
+handleChange(event) {
+   this.setState({
+     content: event.target.value,
+     username: this.props.user,
+     sentAt: this.firebase.database.ServerValue.TIMESTAMP,
+     roomId: this.props.activeRoom.key
+   });
+ }
+
+handleMessageChange(event){
+    this.setState({newMessageContent:event.target.value})
+  }
+
+createMessage(value) {
+  this.messagesRef.push({
+  content: this.state.newMessageContent,
+  username: this.props.user.displayName,
+  sentAt: this.firebase.database.ServerValue.TIMESTAMP,
+  roomId: this.props.activeRoom.key
+  });
+  this.setState({newMessageContent:''})
+}
+
 render() {
     return (
       <div className="message-list">
@@ -28,6 +60,14 @@ render() {
          {this.state.messages.filter(message => message.roomId === this.props.activeRoom.key).map((message, index) =>
            <li key={index}>{message.username}: {message.content} | Sent at: {message.sentAt}</li>
          )}
+
+         <form onSubmit={(event) => { this.createNewMessage(event) }}>
+           <label>
+           Message:
+           <input type="text" placeholder="Say something cool" value={this.state.newMessageContent} onChange={this.handleMessageChange} />
+           </label>
+           <input type="submit" value="Submit" />
+         </form>
       </div>
     )
   }
